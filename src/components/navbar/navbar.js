357434256dea8d7c8311e3af78645a14e8d9d9e5
DIEGOSOUSA <sdiego478@gmail.com>
2023-06-06 08:26:45 -0100

    changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,14 +9,14 @@ function Navbar(props){
     return(
         <div>
             <nav>
-                <a href="#" className="logo"><p>MindelCine</p></a>
+                <p ><p>MindelCine</p></p>
                 <div>
                     <ul id="navbar-controler" className={clicked?".navbar-controler active":".navbar-controler"}>
-                        <li><a onClick={()=>navigate("/")}>Home</a></li>
-                        <li><a onClick={()=>navigate("/about")}>About</a></li>
-                        <li><a onClick={()=>navigate("/contact")}>Contact</a></li>
-                        <li><a onClick={()=>navigate(props.adm?"/adicionar":"")}>{props.adm?"Adicionar":""}</a></li>
-                        <li><a onClick={()=>navigate(props.login?"/perfil":"/login")}>{props.login?"Perfil":"LogIn"}</a></li>
+                        <li><p onClick={()=>navigate("/")}>Home</p></li>
+                        <li><p onClick={()=>navigate("/about")}>About</p></li>
+                        <li><p onClick={()=>navigate("/contact")}>Contact</p></li>
+                        <li><p onClick={()=>navigate(props.adm?"/adicionar":"")}>{props.adm?"Adicionar":""}</p></li>
+                        <li><p onClick={()=>navigate(props.login?"/perfil":"/login")}>{props.login?"Perfil":"LogIn"}</p></li>
                     </ul>
                     <div id="mobile" onClick={clickControler}>
                             <i className={clicked?"fas fa-times":"fas fa-bars"}></i>
